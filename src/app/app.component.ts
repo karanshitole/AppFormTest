43c6a5a0bedf0c957610c6abe5a291889c263908
karanshitole <148190165+karanshitole@users.noreply.github.com>
2024-04-21 14:51:30 +0530

    work on form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AppFormTest';
+  mocArr =[
+    {contactMode:"Email", id:123},
+    {contactMode:'Phone', id:124}
+  ]
+  userDetails={
+    "userInfo": {
+        "Username": "karan",
+        "Email": "shitolejanardhan11@gmail.com"
+    },
+    "secretQuection": "favouratesport",
+    "answer": "chess",
+    "isSubscribed": true,
+    "contactMode": "Email"
+}
+  @ViewChild ("signup") signup !:NgForm
+ constructor(){
+
+ }
+  ngOnInit(): void {
+    
+  }
+  onsignup(signup:NgForm){
+    console.log(signup)
+    console.log(signup.value)
+  }
+  onEdit(){
+    this.signup.form.patchValue(this.userDetails)
+  }
 }
